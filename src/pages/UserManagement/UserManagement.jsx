795bd74794fc8fea6d7f12207238fd07360d248e
Diandria Xavier <diandriaxavier@gmail.com>
2023-05-18 20:37:27 -0300

    feat: add list users

--- src/pages/UserManagement/UserManagement.jsx
+++ src/pages/UserManagement/UserManagement.jsx
@@ -1,46 +1,19 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./UserManagement.css";
-
 import Filters from "../../components/filters/Filters";
 import UserList from "../../components/user_list/UserList";
 
 export default function UserManagement() {
-  // const url = 'https://mack-webmobile.vercel.app/api/users'
+  const [users, setUsers] = useState([]);
 
-  // const user_request = fetch(url, {
-  //   method: 'GET',
-  // })
-  //   .then(response => response.json())
-  //   .then(responseData => {
-  //     console.log('Resposta:', responseData);
-  //   })
-  //   .catch(error => {
-  //     console.log('Erro na requisição:', error);
-  //   });
-
-  const users = [
-    {
-      _id: "645861f71302d474ea43a2d4",
-      name: "joaozinho Abc",
-      avatar: "https://randomuser.me/api/portraits/women/47.jpg",
-      email: "jkjdnf@sokjf.com",
-      salary: 236,
-      date: "2022-12-12",
-      status: "Active",
-      __v: 0,
-    },
-    {
-      _id: "646195d5f72cc33ef5e2e9e6",
-      name: "Renato Cariani",
-      avatar:
-        "https://pbs.twimg.com/profile_images/1519280000385626112/uByzDEB-_400x400.jpg",
-      email: "renato@cariani.com",
-      salary: 10000000,
-      date: "2023-05-14",
-      status: "Inactive",
-      __v: 0,
-    },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://mack-webmobile.vercel.app/api/users");
+      const data = await response.json();
+      setUsers(data);
+    }
+    fetchData()
+  }, [])
 
   return (
     <div className="userManagement">
